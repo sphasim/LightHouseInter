e8688a8382505e58a46e3b44ef0b14b0e27d7247
Siphamandla Simelane <5253510+sphasim@users.noreply.github.com>
2022-11-08 12:55:22 -0700

    first commit

---
+++ lighthouserc.js
@@ -0,0 +1,20 @@
+﻿module.exports = {
+    ci: {
+        collect: {
+            /* Add configuration here */
+            startServerCommand: 'npm run start',
+            url: ['https://localhost:7139/'],
+            numberOfRuns: 5
+        },
+        assert: {
+            assertions: {
+                'categories:performance': ['warn', { minScore: 1 }],
+                'categories:accessibility': ['error', { minScore: 1 }]
+            }
+        },
+        upload: {
+            /* Add configuration here */
+            target: 'temporary-public-storage',
+        },
+    },
+};
